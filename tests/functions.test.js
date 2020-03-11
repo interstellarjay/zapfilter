aa619a9125d1700795431740ac92329f294f640a
Jay <10155824+interstellarjay@users.noreply.github.com>
2020-03-11 15:39:37 +0000

    Update functions.test.js
    Updated tests

--- tests/functions.test.js
+++ tests/functions.test.js
@@ -1,85 +1,160 @@
-import test from 'ava';
-import zapfilter from '../index';
+import zapfilter from 'zapfilter'
+const zf = new zapfilter()
 
-const zf = new zapfilter();
-const dataSet = [
-	{
-		name: 'Julia',
-		eyes: 'brown',
-		age: 24
-	},
-	{
-		name: 'Master Raven',
-		eyes: 'brown',
-		age: 36
-	},
-	{
-		name: 'Nina Williams',
-		eyes: 'blue',
-		age: 40
-	},
-	{
-		name: 'Leo Kliesen',
-		eyes: 'green',
-		age: 19
-	}
-];
-const filtersA = [
-	{
-		filter: zf.filterPartialMatch,
-		onProperty: 'name',
-		condition: 'Leo'
-	},
-	{
-		filter: zf.filterLessThan,
-		onProperty: 'age',
-		condition: 40
-	}
-];
-const filtersB = [
-	{
-		filter: zf.filterEqualTo,
-		onProperty: 'eyes',
-		condition: 'brown'
-	},
-	{
-		filter: zf.filterPartialMatch,
-		onProperty: 'name',
-		condition: 'Leo'
-	},
-	{
-		filter: zf.filterLessThan,
-		onProperty: 'age',
-		condition: 40
-	}
-];
 
-test('Filters using AND logic works correctly', t => {
-	zf.applyFilters(filtersA);
-	const result = zf.filter(dataSet);
-	// Expected result
-	const expected = [
-		{
-			name: 'Leo Kliesen',
-			eyes: 'green',
-			age: 19
-		}
-	];
-	zf.clearFilters();
-	// Assert result matches expected
-	t.deepEqual(result, expected);
-});
+const mockData = [
+  {
+    name: 'IBM (The International Business Machines Corporation)',
+    founded: '1911-06-16',
+    ceo: 'Virginia Marie Rometty',
+    sexofceo: 'female',
+    stock: 'IBM',
+    service: 'Technology company',
+    defunct: false,
+  },
+  {
+    name: 'Microsoft Corporation',
+    founded: '1975-04-04',
+    ceo: 'Satya Nadella',
+    sexofceo: 'male',
+    stock: 'MSFT',
+    service: 'Technology company',
+    defunct: false,
+  },
+  {
+    name: 'Google LLC',
+    founded: '1998-09-04',
+    ceo: 'Sundar Pichai',
+    sexofceo: 'male',
+    stock: 'GOOGL',
+    service: 'Technology company',
+    defunct: false,
+  },
+  {
+    name: 'Astoria Bank (Astoria Financial Corporation)',
+    founded: '1888-01-01',
+    ceo: 'Satya Nadella',
+    sexofceo: 'male',
+    stock: 'AF',
+    service: 'Financial services',
+    defunct: true,
+  },
+]
 
-test('Filters using OR logic works correctly', t => {
-	zf.applyFilters(filtersB);
-	const result = zf.filterOR(dataSet);
-	// Expected result
-	const expected = [
-		{ name: 'Julia', eyes: 'brown', age: 24 },
-		{ name: 'Master Raven', eyes: 'brown', age: 36 },
-		{ name: 'Leo Kliesen', eyes: 'green', age: 19 }
-	];
-	zf.clearFilters();
-	// Assert result matches expected
-	t.deepEqual(result, expected);
-});
+describe('Utils for batch filtering', () => {
+  it('filters all content by logical OR', () => {
+    let filters = [
+      {
+        filter: zf.filterEqualTo,
+        onProperty: 'service',
+        value: 'Financial services',
+      },
+      {
+        filter: zf.filterEqualTo,
+        onProperty: 'service',
+        value: 'Hypermarket retail',
+      },
+    ]
+    zf.setFilters(filters)
+    const result = zf.filter(mockData)
+    zf.clearFilters()
+    expect(result).toEqual([
+      {
+        name: 'Astoria Bank (Astoria Financial Corporation)',
+        founded: '1888-01-01',
+        ceo: 'Satya Nadella',
+        sexofceo: 'male',
+        stock: 'AF',
+        service: 'Financial services',
+        defunct: true,
+      },
+    ])
+  })
+  it('filters all content by logical AND', () => {
+    let filters = [
+      {
+        filter: zf.filterBeforeDate,
+        onProperty: 'founded',
+        value: '1900-01-01',
+      },
+      {
+        filter: zf.filterEqualTo,
+        onProperty: 'sexofceo',
+        value: 'female',
+      },
+    ]
+    zf.setFilters(filters)
+    const result = zf.filterAND(mockData)
+    zf.clearFilters()
+    expect(result).toStrictEqual([])
+  })
+  it('filters all content by greater than date', () => {
+    let filters = [
+      {
+        filter: zf.filterAfterDate,
+        onProperty: 'founded',
+        value: '1990-01-01',
+      },
+    ]
+    zf.setFilters(filters)
+    const result = zf.filter(mockData)
+    zf.clearFilters()
+    expect(result).toEqual([
+      {
+        name: 'Google LLC',
+        founded: '1998-09-04',
+        ceo: 'Sundar Pichai',
+        sexofceo: 'male',
+        stock: 'GOOGL',
+        service: 'Technology company',
+        defunct: false,
+      },
+    ])
+  })
+  it('filters all content by combination of date and boolean value', () => {
+    let filters = [
+      {
+        filter: zf.filterBeforeDate,
+        onProperty: 'founded',
+        value: '1930-01-01',
+      },
+      {
+        filter: zf.filterEqualTo,
+        onProperty: 'defunct',
+        value: false,
+      },
+    ]
+    zf.setFilters(filters)
+    const result = zf.filterAND(mockData)
+    zf.clearFilters()
+    expect(result).toEqual([
+        {
+            name: 'IBM (The International Business Machines Corporation)',
+            founded: '1911-06-16',
+            ceo: 'Virginia Marie Rometty',
+            sexofceo: 'female',
+            stock: 'IBM',
+            service: 'Technology company',
+            defunct: false,
+        },
+    ])
+  })
+  it('filters are properly reset', () => {
+    let filters = [
+      {
+        filter: zf.filterNotEqualTo,
+        onProperty: 'service',
+        value: 'Financial services',
+      },
+      {
+        filter: zf.filterEqualTo,
+        onProperty: 'sexofceo',
+        value: 'female',
+      },
+    ]
+    zf.setFilters(filters)
+    const result = zf.filterAND(mockData)
+    zf.clearFilters()
+    expect(zf.getTotalFilters()).toEqual(0)
+  })
+})
