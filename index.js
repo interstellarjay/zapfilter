e4424e4ae1206c5569628bc225d3eb1a748b1386
Jay <10155824+interstellarjay@users.noreply.github.com>
2020-03-11 14:43:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -99,7 +99,6 @@ class ZapFilter {
       const results = this.combineResultsAndRemoveDuplicates(
         flattenedResultsWithPotentialDuplicates
       )
-      console.log('@@@@', results)
       return results
     }
   
